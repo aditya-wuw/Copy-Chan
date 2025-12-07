acb961824430f09e3bc7844c700d5407c03e99aa
adi <guddumandal605605@gmail.com>
2025-12-07 21:58:47 +0530

    added system tray
    -made the app start minimized
    -made the option to quite the app or show it from the system tray
    -added custom test icon
    -added global shortcut to listen and opent the window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{AppHandle, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use crate::copy_logic::{
     cblisten::cblisten, copy::copy_history_add, copy::del_entry, copy::get_history,
@@ -13,8 +15,35 @@ fn listen_to_clipbord(app: &mut tauri::App) {
         cblisten(handle);
     });
 }
+#[tauri::command]
+fn hide_window(app: AppHandle) {
+    println!("hiding the window");
+    if let Some(main_window) = app.get_webview_window("main") {
+        main_window.hide().unwrap();
+    } else {
+        println!("No window labeled 'main' found");
+    }
+}
+
+#[tauri::command]
+fn close_programe(app_handle: AppHandle) {
+    println!("closing programe");
+    app_handle.exit(0);
+}
+
+#[tauri::command]
+fn show_window(app: AppHandle) {
+    println!("window will show");
+    if let Some(main_window) = app.get_webview_window("main") {
+        main_window.show().unwrap();
+        main_window.set_focus().unwrap();
+    } else {
+        println!("No window labeled 'main' found");
+    }
+}
+
 //opens the window depending on a specific shortcut
-fn open_window_on_global_shortcut(app: &mut tauri::App) {
+fn open_window_on_global_shortcut(_app: &mut tauri::App) {
     // add the logic
 }
 
@@ -23,6 +52,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::default().build())
         .setup(|app| {
             listen_to_clipbord(app);
             open_window_on_global_shortcut(app);
@@ -31,7 +61,10 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             copy_history_add,
             del_entry,
-            get_history
+            get_history,
+            close_programe,
+            show_window,
+            hide_window
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
